f776c074080fd0f9e5b804a81b50ef2b8c22596e
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2025-04-18 13:11:22 -0500

    udpate keymap

--- nvim/.config/nvim/lua/custom/plugins/codium.lua
+++ nvim/.config/nvim/lua/custom/plugins/codium.lua
@@ -2,13 +2,13 @@ return {
 	"Exafunction/codeium.vim",
 	config = function()
 		vim.g.codeium_disable_bindings = 1
-		vim.keymap.set("i", "<M-l>", function()
+		vim.keymap.set("i", "<M-Tab>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<M-;>", function()
+		vim.keymap.set("i", "<S-Tab>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<M-,>", function()
+		vim.keymap.set("i", "<C-Tab>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true, silent = true })
 		vim.keymap.set("i", "<M-x>", function()
